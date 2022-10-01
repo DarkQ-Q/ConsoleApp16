333453555007d138ff116d5c8106e55e3fca4bcd
Павличенко Сергей <DarkProgerr@yandex.ru>
2022-10-01 15:33:33 +0300

    *

---
+++ ConsoleApp16/hello.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    internal class hello
+    {
+        public int Health { get; set; }
+        public int Health1 { get; set; }
+        public int Health2 { get; set; }
+
+    }
+}
